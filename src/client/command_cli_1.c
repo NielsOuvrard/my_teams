a706e4c5165c6e59ab4efdd9141ff955103dcf04
Morais <gabriel.de-souza-morais@epitech.eu>
2023-04-04 14:16:17 +0200

    [fix] French word

--- src/client/command_cli_1.c
+++ src/client/command_cli_1.c
@@ -13,8 +13,8 @@ int login_function          (client *cli, char **array)
     cli->is_connected = 1;
     cli->uuid = strdup(array[1]);
     cli->name = strdup(array[2]);
-    fct_2 fonction = cli->data_lib[0].fct;
-    return fonction(array[1], array[2]);
+    fct_2 function = cli->data_lib[0].fct;
+    return function(array[1], array[2]);
 }
 
 // 1 = client_event_logged_out, type 3
@@ -26,8 +26,8 @@ int logout_function         (client *cli, char **array)
         free(cli->uuid);
         free(cli->name);
     }
-    fct_2 fonction = cli->data_lib[1].fct;
-    return fonction(array[1], array[2]);
+    fct_2 function = cli->data_lib[1].fct;
+    return function(array[1], array[2]);
 }
 
 // 7 = client_print_users
@@ -40,8 +40,8 @@ int users_function          (client *cli, char **array)
         char *uuid = array[i];
         char *name = array[i + 1];
         char *status = array[i + 2];
-        fct_8 fonction = cli->data_lib[7].fct;
-        fonction(uuid, name, atoi(status));
+        fct_8 function = cli->data_lib[7].fct;
+        function(uuid, name, atoi(status));
     }
     return 0;
 }
@@ -50,8 +50,8 @@ int users_function          (client *cli, char **array)
 // client_error_unknown_user
 int user_function           (client *cli, char **array)
 {
-    fct_8 fonction = cli->data_lib[19].fct;
-    return fonction(array[1], array[2], atoi(array[3]));
+    fct_8 function = cli->data_lib[19].fct;
+    return function(array[1], array[2], atoi(array[3]));
 }
 
 int send_function           (client *cli, char **array)
