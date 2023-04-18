57eea50b19e6548b37429721e06cb5f76bd37284
Niels <niels.ouvrard@epitech.eu>
2023-04-18 14:56:21 +0000

    list in list

--- src/client/command_cli_list.c
+++ src/client/command_cli_list.c
@@ -9,46 +9,67 @@
 
 int list_team_function (client *cli, char **array)
 {
-    if (array[1] == NULL || array[2] == NULL || array[3] == NULL)
-        return 0;
-    client_print_teams(array[1], array[2], array[3]);
+    for (int i = 1; array[i] != NULL; i += 3) {
+        if (!array[i + 1] || !array[i + 2])
+            return 0;
+        char *team_uuid = array[i];
+        char *team_name = array[i + 1];
+        char *team_description = array[i + 2];
+        client_print_teams(team_uuid, team_name, team_description);
+    }
     return 0;
 }
 
 int list_channel_function (client *cli, char **array)
 {
-    if (array[1] == NULL || array[2] == NULL || array[3] == NULL)
-        return 0;
-    client_team_print_channels(array[1], array[2], array[3]);
+    for (int i = 1; array[i] != NULL; i += 3) {
+        if (!array[i + 1] || !array[i + 2])
+            return 0;
+        char *channel_uuid = array[i];
+        char *channel_name = array[i + 1];
+        char *channel_description = array[i + 2];
+        client_team_print_channels(channel_uuid, channel_name, channel_description);
+    }
     return 0;
 }
 
 int list_thread_function (client *cli, char **array)
 {
-    if (array[1] == NULL || array[2] == NULL || array[3] == NULL ||
-        array[4] == NULL || array[5] == NULL)
-        return 0;
-    struct tm tm;
-    strptime(array[3], "%a %b %d %H:%M:%S %Y", &tm);
-    time_t t = mktime(&tm);
-    if (t == -1) {
-        return 0;
+    for (int i = 1; array[i] != NULL; i += 5) {
+        if (!array[i + 1] || !array[i + 2] || !array[i + 3] || !array[i + 4])
+            return 0;
+        char *t_uuid = array[i];
+        char *user_uuid = array[i + 1];
+        char *t_timestamp = array[i + 2];
+        char *t_title = array[i + 3];
+        char *t_body = array[i + 4];
+        struct tm tm;
+        strptime(t_timestamp, "%a %b %d %H:%M:%S %Y", &tm);
+        time_t t = mktime(&tm);
+        if (t == -1) {
+            return 0;
+        }
+        client_channel_print_threads(t_uuid, user_uuid, t, t_title, t_body);
     }
-    client_channel_print_threads(array[1], array[2], t, array[4], array[5]);
     return 0;
 }
 
 int list_reply_function (client *cli, char **array)
 {
-    if (array[1] == NULL || array[2] == NULL || array[3] == NULL ||
-        array[4] == NULL)
-        return 0;
-    struct tm tm;
-    strptime(array[3], "%a %b %d %H:%M:%S %Y", &tm);
-    time_t t = mktime(&tm);
-    if (t == -1) {
-        return 0;
+    for (int i = 1; array[i] != NULL; i += 4) {
+        if (!array[i + 1] || !array[i + 2] || !array[i + 3])
+            return 0;
+        char *thread = array[i];
+        char *user = array[i + 1];
+        char *time = array[i + 2];
+        char *body = array[i + 3];
+        struct tm tm;
+        strptime(time, "%a %b %d %H:%M:%S %Y", &tm);
+        time_t t = mktime(&tm);
+        if (t == -1) {
+            return 0;
+        }
+        client_thread_print_replies(thread, user, t, body);
     }
-    client_thread_print_replies(array[1], array[2], t, array[4]);
     return 0;
 }
